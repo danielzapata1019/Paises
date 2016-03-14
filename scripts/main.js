311f48cd7d64f8acc9bccb78bb9084e8fffe769f
danielzapata1019 <danielzapata-1019@hotmail.com>
2016-03-13 21:59:14 -0500

    Modal detail

--- scripts/main.js
+++ scripts/main.js
@@ -4,23 +4,22 @@
 
 "use strict";
 
-console.log("ya tengo el main");
+var country=[];
 
-var tal = "hola";
-console.log(tal);
-var modal=document.getElementById('myModal');
 //https://restcountries.eu/rest/v1/alpha/co
 
 $.ajax( "https://restcountries.eu/rest/v1/all" )
     .done(function(info) {
         for(var i=0; i< info.length; i++) {
             var datos = info[i];
-            console.log(datos);
+            country=info[i];
+            var posicion;
+            //console.log(country);
             $('.list-countries').append('' +
                 '<div class="col-md-4">'+
                 '<div class="country">'+
                 '<div class="flag"> ' +
-                '<img src="assets/flags/'+ datos.alpha2Code +'.png" onclick="masInfo()"/> ' +
+                '<img src="assets/flags/'+ datos.alpha2Code +'.png" onclick="masInfo('+datos.alpha2Code+');"/> ' +
                 '</div> ' +
                 '<div class="info"> ' +
                 '<div>Nombre: <span>' + datos.name + '</span></div> ' +
@@ -35,8 +34,44 @@ $.ajax( "https://restcountries.eu/rest/v1/all" )
         console.log(err);
     });
 
-function masInfo() {
+function masInfo(code) {
     $('#myModal').modal()                      // initialized with defaults
     $('#myModal').modal({ keyboard: false })   // initialized with no keyboard
     $('#myModal').modal('show')
+    console.log(code);
+
+    for(var j=0; j < country.length ; j++){
+        if(country.alpha2Code==code){
+            console.log(country[j]);
+            $('.detail').append('' +
+                '<div class="col-sm-6 small">'+
+                '<div>Region: <span>' + country.region + '</span></div>'+
+                '<div>Subregion: <span>' + country.subregion + '</span></div>'+
+                '<div>Area:<span>' + country.area + '</span></div>'+
+                '</div>'+
+                '<div class="col-sm-6 small">'+
+                ' <div>Idiomas: <span>' + country.languages + '</span></div>'+
+                '<div>Codigo Postal: <span>' + country.callingCodes + '</span></div>'+
+                '<div>Timezones:<span>' + country.timezones + '</span></div>'+
+                '</div>');
+        }else {
+            $('.detail').append('' +
+                '<div class="col-sm-12 small text-center">'+
+                '<div>No se ha podido encontrar mas información sobre el pais seleccionado</div>'
+            );
+        }
+
+
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
